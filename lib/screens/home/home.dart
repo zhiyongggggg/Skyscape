4d4ea02032e0f79b357de9279b535516edfc81cc
Zhi Yong <93578721+zhiyongggggg@users.noreply.github.com>
2024-03-17 01:23:50 +0800

    :hammer: Include calculated quality

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,7 +8,6 @@ import 'package:skyscape/services/auth.dart';
 import 'package:skyscape/services/database.dart';
 import 'dictionaries.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -21,24 +20,46 @@ class _HomeState extends State<Home> {
   Map<String, dynamic?> allValues = {};
   Map<String, dynamic?> psiValues = {};
   Map<String, Map<String, dynamic>> filteredStations = {};
-  List<String> favouritedLocationNames = ['Admiralty', 'Ang Mo Kio', 'Pasir Ris', 'Yew Tee']; // default names
-  
+  List<String> favouritedLocationNames = [
+    'Admiralty',
+    'Ang Mo Kio',
+    'Pasir Ris',
+    'Yew Tee'
+  ]; // default names
+
   int currentIndex = 0;
 
-  void getData(List<String> favouritedLocationNames) async {
-    String url, longitude, latitude;
-
+  void getData(List<String> favouritedLocationNames, String date) async {
+    String url, longitude, latitude, sunSet;
+    double sunsetQuality, humidityQuality, cloudCoverQuality, PSI, PSIRating, PSIQuality;
     for (var location in favouritedLocationNames) {
       latitude = locationToCoordinatesMapping[location][0].toString();
       longitude = locationToCoordinatesMapping[location][1].toString();
 
-      url = 'https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,cloud_cover&daily=sunrise,sunset&timezone=Asia%2FSingapore&forecast_days=1';
+      // Get sunset timing first, the weather conditions will be forecasted based on this timing later on.
+      url =
+          'https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=sunrise,sunset&timezone=Asia%2FSingapore&start_date=${date}&end_date=${date}';
       http.Response response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         Map data = jsonDecode(response.body);
-        Map<String, dynamic> currentConditions = data["current"];
-        String sunSet = data["daily"]["sunset"][0].substring(11); // Use substring() to exclude the date from the string, only leaving with time
-        allValues[location] = [currentConditions["temperature_2m"], currentConditions["relative_humidity_2m"], currentConditions["cloud_cover"], sunSet];
+        sunSet = data["daily"]["sunset"][0].substring(
+            11); // Use substring() to exclude the date from the string, only leaving with time
+
+        // Now using the sunset timing, we find the weather conditions
+        url = 'https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,cloud_cover&timezone=Asia%2FSingapore&start_hour=${date}T${sunSet}&end_hour=${date}T${sunSet}';
+        response = await http.get(Uri.parse(url));
+        if (response.statusCode == 200) {
+          Map data = jsonDecode(response.body);
+          Map<String, dynamic> currentConditions = data["hourly"];
+          allValues[location] = [
+            currentConditions["temperature_2m"],
+            currentConditions["relative_humidity_2m"][0],
+            currentConditions["cloud_cover"][0],
+            sunSet
+          ];
+        } else {
+          print('Failed to fetch data: ${response.statusCode}');
+        }
       } else {
         print('Failed to fetch data: ${response.statusCode}');
       }
@@ -48,19 +69,43 @@ class _HomeState extends State<Home> {
       response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         Map<String, dynamic> data = jsonDecode(response.body);
-        Map<String, dynamic> readings = data['items'][0]['readings']; 
+        Map<String, dynamic> readings = data['items'][0]['readings'];
         String? region = locationToRegionMapping[location];
-        print(region);
-        psiValues[location] = readings['psi_twenty_four_hourly'][region]; // WORK HERE REQUIRED!!! need to map location to their respective regions
+        allValues[location].add(readings['psi_twenty_four_hourly'][region]);
         // print(psiValues); ---> {west: 49, east: 61, central: 54, south: 51, north: 38}
         setState(() {});
       } else {
         print('Failed to fetch data: ${response.statusCode}');
       }
+      cloudCoverQuality = allValues[location][2] * 0.4;
+      humidityQuality = allValues[location][1] * 0.3;
+      PSI = allValues[location][4].toDouble();
+      if (PSI <= 55) {
+        PSIQuality = 80 + ((55 - PSI)/55 * 20);
+      } else {
+        PSIQuality = 20 + ((250 - PSI)/250 * 80);
+      }
+      PSIQuality = PSIQuality * 0.3;
+      sunsetQuality = PSIQuality + humidityQuality + PSIQuality;
+      allValues[location].add(sunsetQuality.toStringAsFixed(2));
     }
   }
 
+  String _getCurrentDateInSingapore() {
+    DateTime now = DateTime.now().toUtc(); // Getting current time in UTC
+    DateTime singaporeTime = now.add(const Duration(
+        hours: 8)); // Adding 8 hours to convert to Singapore time
+    String formattedDate =
+        '${singaporeTime.year}-${_formatDateComponent(singaporeTime.month)}-${_formatDateComponent(singaporeTime.day)}';
+    return formattedDate;
+  }
+
+  String _formatDateComponent(int component) {
+    return component < 10 ? '0$component' : '$component';
+  }
+
   @override
+<<<<<<< Updated upstream
 void initState() {
   super.initState();
   _getFavouritedLocations();
@@ -79,13 +124,36 @@ Future<void> _getFavouritedLocations() async {
   });
   getData(favouritedLocationNames);
 }
+=======
+  void initState() {
+    super.initState();
+    String currentDate =
+        _getCurrentDateInSingapore(); // TODO: Date change according to calender
+    getData(favouritedLocationNames, currentDate);
+    _getFavouritedLocations();
+  }
+
+  Future<void> _getFavouritedLocations() async {
+    List<String> locations = await DatabaseService(uid: _auth.currentUser!.uid)
+        .getFavouritedLocations();
+    setState(() {
+      favouritedLocationNames = locations;
+    });
+    String currentDate =
+        _getCurrentDateInSingapore(); // TODO: Date change according to calender
+    getData(favouritedLocationNames, currentDate);
+  }
+
+>>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[50],
       appBar: AppBar(
         title: const Text('Skyscape'),
-        backgroundColor: currentIndex == 3 ? Color.fromARGB(255, 189, 235, 191) : Colors.amber[400],
+        backgroundColor: currentIndex == 3
+            ? Color.fromARGB(255, 189, 235, 191)
+            : Colors.amber[400],
         elevation: 0.0,
         actions: <Widget>[
           TextButton.icon(
@@ -155,6 +223,12 @@ Future<void> _getFavouritedLocations() async {
                       child: Text('$location : Temp-${allValues[location]?[0]}, Humidity-${allValues[location]?[1]}%, CC-${allValues[location]?[2]}%, PSI-${psiValues[location]}%, Sunset: ${allValues[location]?[3]}'),                  ),
                   ],
                 ),
+<<<<<<< Updated upstream
+=======
+                child: Text(
+                    '$location : Temp-${allValues[location]?[0][0]}, Humidity-${allValues[location]?[1]}%, CC-${allValues[location]?[2]}%, PSI-${allValues[location]?[4]}, Sunset: ${allValues[location]?[3]}, Sunset Quality: ${allValues[location]?[5]}'),
+              ),
+>>>>>>> Stashed changes
             ],
           ),
         ),
