dbf84d5fda673755ceff046787ac6f91723963db
Yaohao <106961008+xiGHK@users.noreply.github.com>
2024-03-25 18:20:40 +0800

    :hammer: implemented following tabs in feed

--- lib/screens/Feed/feed.dart
+++ lib/screens/Feed/feed.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skyscape/screens/Feed/uploadpicture.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:skyscape/services/database.dart';
+import 'package:skyscape/screens/Feed/following.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
@@ -10,14 +11,22 @@ class FeedPage extends StatefulWidget {
   State<FeedPage> createState() => _FeedPageState();
 }
 
-class _FeedPageState extends State<FeedPage> {
+class _FeedPageState extends State<FeedPage> with SingleTickerProviderStateMixin {
   List<Map<String, dynamic>> _photos = [];
   final _databaseService = DatabaseService();
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
     _fetchPhotos();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   Future<void> _fetchPhotos() async {
@@ -40,87 +49,104 @@ class _FeedPageState extends State<FeedPage> {
       _photos = photos;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    //  appBar: AppBar(
-        //title: const Text('Photos uploaded by other users'),
-        //centerTitle: true,
-        //backgroundColor: Colors.OR[200],
-     // ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              const Color.fromARGB(255, 255, 225, 181)!,
-              Colors.orange[100]!,
-              Color.fromARGB(255, 243, 213, 245)!,
-              Color.fromARGB(255, 250, 217, 182)!,
-            ],
-            stops: [0.1, 0.3, 0.5, 0.8],
+      backgroundColor: Colors.orange[200],
+      body: Column(
+        children: [
+          Container(
+            color: Colors.orange[200],
+            child: TabBar(
+              controller: _tabController,
+              tabs: const [
+                Tab(text: 'For you'),
+                Tab(text: 'Following'),
+              ],
+            ),
           ),
-        ),
-        child: _photos.isNotEmpty
-            ? SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(40.0),
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1,
-                      crossAxisSpacing: 16,
-                      mainAxisSpacing: 16,
-                      childAspectRatio: 0.8,
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        const Color.fromARGB(255, 255, 225, 181)!,
+                        Colors.orange[100]!,
+                        Color.fromARGB(255, 243, 213, 245)!,
+                        Color.fromARGB(255, 250, 217, 182)!,
+                      ],
+                      stops: [0.1, 0.3, 0.5, 0.8],
                     ),
-                    itemCount: _photos.length,
-                    itemBuilder: (context, index) {
-                      final photo = _photos[index];
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Posted by ' + photo['username'],
-                            style: TextStyle(
-                              fontWeight: FontWeight.normal,
-                              color: Colors.grey[800],
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Expanded(
-                            child: Container(
-                              height: 200,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.1),
-                                    blurRadius: 4,
-                                    offset: const Offset(0, 2),
-                                  ),
-                                ],
-                                image: DecorationImage(
-                                  image: NetworkImage(photo['url']),
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      );
-                    },
                   ),
+                  child: _photos.isNotEmpty
+                      ? SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.all(40.0),
+                            child: GridView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 1,
+                                crossAxisSpacing: 16,
+                                mainAxisSpacing: 16,
+                                childAspectRatio: 0.8,
+                              ),
+                              itemCount: _photos.length,
+                              itemBuilder: (context, index) {
+                                final photo = _photos[index];
+                                return Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Posted by ' + photo['username'],
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.grey[800],
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Expanded(
+                                      child: Container(
+                                        height: 200,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(8),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.black.withOpacity(0.1),
+                                              blurRadius: 4,
+                                              offset: const Offset(0, 2),
+                                            ),
+                                          ],
+                                          image: DecorationImage(
+                                            image: NetworkImage(photo['url']),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                          ),
+                        )
+                      : const Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 255, 177, 67)),
+                          ),
+                        ),
                 ),
-              )
-            : const Center(
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 255, 177, 67)),
-                ),
-              ),
+                const FollowingPage(),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -134,4 +160,5 @@ class _FeedPageState extends State<FeedPage> {
       ),
     );
   }
+
 }
---
+++ lib/screens/Feed/following.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FollowingPage extends StatelessWidget {
+  const FollowingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Following Page'),
+      ),
+    );
+  }
+}
