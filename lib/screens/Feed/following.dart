a4df249d68c790d09c16893c4a231315117d4cb2
Yaohao <106961008+xiGHK@users.noreply.github.com>
2024-03-25 22:50:53 +0800

    :hammer: simple UI implementation for following tab

--- lib/screens/Feed/following.dart
+++ lib/screens/Feed/following.dart
@@ -4,7 +4,6 @@ import 'package:skyscape/services/auth.dart';
 import 'package:skyscape/services/database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-
 class FollowingPage extends StatefulWidget {
   const FollowingPage({Key? key}) : super(key: key);
 
@@ -66,54 +65,74 @@ class _FollowingPageState extends State<FollowingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _isLoading
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            const Color.fromARGB(255, 255, 225, 181)!,
+            Colors.orange[100]!,
+            Color.fromARGB(255, 243, 213, 245)!,
+            Color.fromARGB(255, 250, 217, 182)!,
+          ],
+          stops: [0.1, 0.3, 0.5, 0.8],
+        ),
+      ),
+      child: _isLoading
           ? Loading()
           : _photos.isNotEmpty
-              ? GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                    childAspectRatio: 0.8,
-                  ),
-                  itemCount: _photos.length,
-                  itemBuilder: (context, index) {
-                    final photo = _photos[index];
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Posted by ${photo['username']}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey[800],
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        Expanded(
-                          child: Container(
-                            height: 200,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.1),
-                                  blurRadius: 4,
-                                  offset: const Offset(0, 2),
-                                ),
-                              ],
-                              image: DecorationImage(
-                                image: NetworkImage(photo['url']),
-                                fit: BoxFit.cover,
+              ? SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        childAspectRatio: 0.8,
+                      ),
+                      itemCount: _photos.length,
+                      itemBuilder: (context, index) {
+                        final photo = _photos[index];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Posted by ${photo['username']}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey[800],
+                                fontSize: 16,
                               ),
                             ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
+                            const SizedBox(height: 8),
+                            Expanded(
+                              child: Container(
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black.withOpacity(0.1),
+                                      blurRadius: 4,
+                                      offset: const Offset(0, 2),
+                                    ),
+                                  ],
+                                  image: DecorationImage(
+                                    image: NetworkImage(photo['url']),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
                 )
               : const Center(
                   child: Text('No photos posted by followed users.'),
